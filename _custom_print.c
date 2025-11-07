d21bce11b5f122ec1bed836039937b8e3eb3b7df
zolldic <ojiip95@gmail.com>
2025-11-07 15:24:55 +0200

    feat: Implement custom function to print char and str to stdout

---
+++ _custom_print.c
@@ -0,0 +1,32 @@
+#include "main.h"
+
+/**
+* print_char - print a character to STDOUT
+* @ap: list of unkown arguments passed to _printf
+* Return: count of bytes printed in STDOUT
+*/
+int print_char(va_list ap)
+{
+	char c =  va_arg(ap, int);
+
+	return (write(STDOUT, &c, 1));
+}
+
+/**
+* print_str - print a string to STDOUT
+* @ap: list of unkown arguments passed to _printf
+* Return: count of bytes printed in STDOUT
+*/
+int print_str(va_list ap)
+{
+	int count = 0;
+	char *str = va_arg(ap, char *);
+
+	while (*str != '\0')
+	{
+		count += write(STDOUT, str, 1);
+		str++;
+	}
+
+	return (count);
+}
