df7116b58a7a59d08f9332141b832ee030da71d3
zolldic <ojiip95@gmail.com>
2025-11-07 15:30:09 +0200

    feat: Adds specifier struct

--- main.h
+++ main.h
@@ -6,6 +6,30 @@
 #include <unistd.h>
 #include <stdio.h>
 
+
+/**
+ * struct Specifiers - a two memeber struct for
+ *	specifiers and custom functions
+ *	@name: name of the specifier
+ *	@func: the specifier custom funciton
+ *
+ *	Description: this struct is used to match the spsecifier
+ *		with its custom funciton in the _handler funciton
+ */
+
+typedef struct Specifiers
+{
+	char name;
+	int (*func)(va_list ap);
+} spec_t;
+
 int _printf(const char *format, ...);
+
+int _handler(char specifier, va_list ap);
+
+/* custom print functions */
+int print_char(va_list ap);
+int print_str(va_list ap);
+
 
 #endif
